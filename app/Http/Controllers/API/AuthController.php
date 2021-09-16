ec9f9b30ef2b2877a76e6228e33ba9bad9565bce
Kiran Makwana <kiranmakwana.nl@gmail.com>
2021-09-16 12:10:28 +0530

    - login,signup

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Validator;
 
 class AuthController extends Controller
 {
@@ -17,20 +20,40 @@ class AuthController extends Controller
         $password = $request->password;
         $user = User::whereEmail($email)->first();
         if ($user) {
-            if (Hash::check($password, $user->password)) {
-//                if (!request()->user()->currentAccessToken()->token)
-//                {
+            if ($user->user_type == 'customer')
+            {
+                if (Hash::check($password, $user->password)) {
                     $token = $user->createToken('auth_token')->plainTextToken;
-//                }
-                return ['token'=>$token];
-            } else {
-                return ['message' => 'Invalid Password'];
+                    return (new UserResource($user))->additional(['token' => $token]);
+                } else {
+                    return ['message' => 'Invalid Password'];
+                }
             }
-        } else {
-            return ['message' => 'Invalid Email'];
         }
+            return ['message' => 'Invalid Email Or Password'];
     }
 
+    public function signup(Request $request): UserResource
+    {
+        #param
+        $email = $request->email;
+        $name = $request->name;
+        $password = $request->password;
+
+        $user = new User;
+        $user->status = 1;
+        $user->email = $email;
+        $user->name = $name;
+        $user->password = $password;
+        $user->save();
+
+        $customer = new Customer;
+        $customer->user_id = $user->id;
+        $customer->save();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return (new UserResource($user))->additional(['token' => $token]);
+    }
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login',[\App\Http\Controllers\API\AuthController::class,'login']);
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::post('/signup',[\App\Http\Controllers\API\AuthController::class,'signup']);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/test',[\App\Http\Controllers\API\AuthController::class,'test']);
+    Route::post('/logout',[\App\Http\Controllers\API\AuthController::class,'logout']);
 });
